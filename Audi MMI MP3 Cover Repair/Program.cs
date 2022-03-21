8a9842f2411a9893e63656567f4533fe88102c72
枫谷剑仙 <mapaler@163.com>
2022-03-21 18:58:56 +0800

    增加一点代码

--- Audi MMI MP3 Cover Repair/Program.cs
+++ Audi MMI MP3 Cover Repair/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using IdSharp.Tagging.ID3v2;
 using IdSharp.Tagging.ID3v2.Frames;
+using System.Drawing;
 
 foreach (var item in args)
 {
@@ -12,6 +13,15 @@ foreach (var item in args)
     Console.WriteLine(string.Format("Pictures:  {0}", id3v2.PictureList.Count));
     IAttachedPicture attachedPicture = id3v2.PictureList[0];
     
+    //保存封面图片
     FileStream fileStream = new FileStream(attachedPicture.Description + "." + attachedPicture.PictureExtension, FileMode.OpenOrCreate, FileAccess.Write);
     fileStream.Write(attachedPicture.PictureData);
+
+    //将封面加载到内存
+    Stream stream = new MemoryStream(attachedPicture.PictureData);
+    Image image = Image.FromStream(stream);
+    
+    Console.WriteLine(string.Format("Picture Type:     {0}", attachedPicture.PictureType));
+    Console.WriteLine(string.Format("Size:     {0}x{1}", image.Size.Width, image.Size.Height));
+    
 }
