3ea0f9218e232fb61260e92b3971ac5b567b6c24
枫谷剑仙 <mapaler@163.com>
2022-03-30 19:10:28 +0800

    做好基本功能

--- Audi MMI MP3 Cover Repair/Program.cs
+++ Audi MMI MP3 Cover Repair/Program.cs
@@ -1,21 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using IdSharp.Tagging.ID3v1;
 using IdSharp.Tagging.ID3v2;
 using IdSharp.Tagging.ID3v2.Frames;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+const uint maxWidth = 480; //图象的最大宽度与高度
+const uint maxHeight = 480;
+const byte quality = 90; //JPEG的图像质量
+
+//获取JPEG的编码器信息
+ImageCodecInfo? jpegEncoder = null;
+foreach (ImageCodecInfo codec in ImageCodecInfo.GetImageDecoders())
+{
+    if (codec.FormatID == ImageFormat.Jpeg.Guid)
+    {
+        jpegEncoder = codec;
+        break;
+    }
+}
 
 foreach (var item in args)
 {
-#if DEBUG
-    Console.WriteLine("参数: {0}", item);
-#endif
-    ID3v2Tag id3v2 = new ID3v2Tag(item);
+    Debug.WriteLine("参数: {0}", item);
+
+    FileInfo musicFile = new(item); //获取目标文件路径信息
+    if (!musicFile.Exists) //如果音乐文件不存在则跳过
+    {
+        Console.Error.WriteLine("错误：未发现音乐文件 {0}", item);
+        continue;
+    }
+    ID3v2Tag id3v2 = new(item);
+
     Console.WriteLine(string.Format("Title:     {0}", id3v2.Title));
     Console.WriteLine(string.Format("Pictures:  {0}", id3v2.PictureList.Count));
     IAttachedPicture attachedPicture = id3v2.PictureList[0];
-    
+
+#if DEBUG
     //保存封面图片
-    FileStream fileStream = new FileStream(attachedPicture.Description + "." + attachedPicture.PictureExtension, FileMode.OpenOrCreate, FileAccess.Write);
+    string coverFileName = musicFile.DirectoryName + "/" + Path.GetFileNameWithoutExtension(musicFile.Name) + "-" + attachedPicture.PictureType + "." + attachedPicture.PictureExtension;
+    FileStream fileStream = new(coverFileName, FileMode.OpenOrCreate, FileAccess.Write);
     fileStream.Write(attachedPicture.PictureData);
+#endif
+
+    //如果不是封面，则清空后重新添加封面
+    if (attachedPicture.PictureType != PictureType.CoverFront)
+    {
+        id3v2.PictureList.Clear();
+        attachedPicture.PictureType = PictureType.CoverFront;
+        id3v2.PictureList.Add(attachedPicture);
+    }
 
     //将封面加载到内存
     Stream stream = new MemoryStream(attachedPicture.PictureData);
@@ -23,5 +58,40 @@ foreach (var item in args)
     
     Console.WriteLine(string.Format("Picture Type:     {0}", attachedPicture.PictureType));
     Console.WriteLine(string.Format("Size:     {0}x{1}", image.Size.Width, image.Size.Height));
-    
+    if (image.Size.Width > maxWidth || image.Size.Height > maxHeight)
+    {
+        double scale;
+        scale = Math.Min((double)maxWidth / (double)image.Size.Width, (double)maxHeight / (double)image.Size.Height);
+        Console.WriteLine(string.Format("缩小比例:     {0}", scale));
+        Size size = new(Convert.ToInt32(image.Size.Width * scale), Convert.ToInt32(image.Size.Height * scale));
+        Image? imgOutput = image.GetThumbnailImage(size.Width, size.Height, null, IntPtr.Zero);
+
+        //创建JPEG压缩的参数
+        EncoderParameters parameters = new(1);
+        parameters.Param[0] = new EncoderParameter(Encoder.Quality, (long)quality);
+
+        //在内存中压缩转存一遍
+        MemoryStream ms = new();
+        imgOutput.Save(ms, jpegEncoder, parameters);
+        Image imgStream = Image.FromStream(ms);
+
+#if DEBUG
+        //保存封面图片
+        string coverThumbnailFileName = musicFile.DirectoryName + "/" + Path.GetFileNameWithoutExtension(musicFile.Name) + "-" + attachedPicture.PictureType + "-resize." + attachedPicture.PictureExtension;
+        imgStream.Save(coverThumbnailFileName);
+        //复制一份新的mp3来修改封面
+        string newFilename = musicFile.DirectoryName + "/" + Path.GetFileNameWithoutExtension(musicFile.Name) + "_id3" + musicFile.Extension;
+        File.Copy(item, newFilename, true);
+#else
+        string newFilename = musicFile.FullName;
+#endif
+        attachedPicture.Picture = imgStream; //将ID3封面修改为新的图片
+
+        id3v2.Save(newFilename);
+    }
+    else
+    {
+        Console.WriteLine("图片大小不需要调整");
+        continue;
+    }
 }
---
+++ README.md
@@ -0,0 +1,12 @@
+奥迪汽车多媒体交互系统 MP3 音乐文件封面修复
+=======
+## 功能
+将 MP3 文件的图片全部缩小到不大于 480x480 ，位置调整到封面(Cover-Front)。
+
+## 运行
+本来 .Net Framwork 也能写，但是为了学习新技术，用了 .Net 6。
+
+因为是个人用，所以懒得写传入参数了。只支持传入 mp3 文件路径。
+
+## 使用开源库
+* [IdSharp](//github.com/RandallFlagg/IdSharpCore)   .NET ID3 Tagging Library
